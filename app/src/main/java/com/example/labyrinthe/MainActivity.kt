51951e9b18f081cd50c93663440f49b32c05ca05
SchrodingersK4t <166732612+SchrodingersK4t@users.noreply.github.com>
2025-04-14 18:00:35 +0200

    Main menu added

--- app/src/main/java/com/example/labyrinthe/MainActivity.kt
+++ app/src/main/java/com/example/labyrinthe/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.labyrinthe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // When opening the app (=onCreate) the executed fun by default is showMainMenu()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showMainMenu()
+    }
+
+    // Definition of the functions that give life to the layouts
+    private fun showMainMenu() {
+        //Sets the content on screen to be the .xml file specified inside brackets
+        setContentView(R.layout.main_menu)
+
+        // Findng the buttons on the main menu's layout
+        val startGameButton = findViewById<Button>(R.id.StartGame)
+        val creditsButton = findViewById<Button>(R.id.Credits)
+
+        // Putting the buttons on listener to execute the function below when clicked
+        startGameButton.setOnClickListener {
+            showGame()
+        }
+
+        creditsButton.setOnClickListener {
+            showCredits()
+        }
+    }
+
+    private fun showGame() {
         setContentView(R.layout.activity_main)
+
+        // Ici tu peux initialiser ton moteur de jeu ou tes éléments
+        // ex : startGameLoop(), initPlayer(), etc.
+    }
+
+    private fun showCredits() {
+        setContentView(R.layout.credits)
+
+        val backButton = findViewById<Button>(R.id.back_main_menu)
+        backButton.setOnClickListener {
+            showMainMenu()
+        }
     }
 }
