c4b94ffdf589467e3b9d19bb5d6347f2ad678a3d
Elie <kolnidrei2020@protonmail.com>
2025-04-15 09:04:17 +0200

    Link Map to Level

--- app/src/main/java/com/example/labyrinthe/Level.kt
+++ app/src/main/java/com/example/labyrinthe/Level.kt
@@ -1,4 +1,29 @@
 package com.example.labyrinthe
 
 class Level {
+    private var isRunning = false
+    init {
+        //Game loop ?
+        //What happens when a new level is created ?
+        val map = Map()
+    }
+
+    /**
+     * Starts the current level's loop. There is no need for real time as it is a simple 2D single-player game.
+     */
+    fun run() {
+        while(isRunning) {
+            //1) Listen to events if any
+            //2) Handle them
+            //3) Update what's shown on the screen.
+            TODO("Implement")
+        }
+    }
+
+    /**
+     * Stops the current level's loop.
+     */
+    fun stop(){
+        isRunning = false
+    }
 }
